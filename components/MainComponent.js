5475bb5d321c525059fc7397cfd2a530e9334273
Adnan Kasem <adnankasem2017@gmail.com>
2021-07-23 21:33:40 -0700

    Navigation part 2 (Drawer Navigation)

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,33 +1,74 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { Text, View } from 'react-native';
-import { CAMPSITES, Campsites } from '../shared/campsites';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Constants from 'expo-constants';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
     }
+);
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+       
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
     }
+)
+
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+)
+
+const AppNavigator = createAppContainer(MainNavigator)
+
+class Main extends Component {
 
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Directory 
-                campsites={this.state.campsites} 
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />;
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
+            <View 
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+                }}
+            >
+                <AppNavigator />
              </View>
         );
     }
